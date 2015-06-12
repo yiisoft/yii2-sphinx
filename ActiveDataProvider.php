3e3dc2df442c5f2d47be0de24033a450ad048a3d
Klimov Paul <klimov.paul@gmail.com>
2015-06-12 17:32:48 +0300

    Fixed extra query on access `meta` or `facet` before `models` at `ActiveDataProvider`

--- ActiveDataProvider.php
+++ ActiveDataProvider.php
@@ -87,7 +87,7 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
     public function getFacets()
     {
         if (!is_array($this->_facets)) {
-            $this->prepareModels();
+            $this->prepare();
         }
         return $this->_facets;
     }
@@ -106,7 +106,7 @@ class ActiveDataProvider extends \yii\data\ActiveDataProvider
     public function getMeta()
     {
         if (!is_array($this->_meta)) {
-            $this->prepareModels();
+            $this->prepare();
         }
         return $this->_meta;
     }
