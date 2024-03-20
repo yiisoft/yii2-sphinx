6de8523dd77878036331f453963ecb97fcbb6646
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-20 12:07:59 +0300

    fix

--- tests/data/actions/sphinx-setup-3.5.1.sh
+++ tests/data/actions/sphinx-setup-3.5.1.sh
@@ -8,6 +8,8 @@ wget https://sphinxsearch.com/files/sphinx-3.5.1-82c60cb-linux-amd64.tar.gz -O /
     && tar -zxf /tmp/sphinxsearch.tar.gz \
     && rm /tmp/sphinxsearch.tar.gz
 
+echo "test"
+
 # make dir that is used in sphinx config
 mkdir -p $PWD/sphinx
 sed -i s\~SPHINX_BASE_DIR~$PWD/sphinx~g $CWD/../sphinx-3.5.1.conf
