9ef00f044a4ccfee239ab4940f2c8927bcb931df
Sergei Predvoditelev <sergei@predvoditelev.ru>
2024-03-20 11:40:28 +0300

    improve

--- tests/data/actions/libmysqlclient20.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# https://askubuntu.com/a/1337909
-echo 'deb http://security.ubuntu.com/ubuntu xenial-security main' | sudo tee /etc/apt/sources.list.d/xenial-security.list
-sudo apt update
-sudo apt install libmysqlclient20
